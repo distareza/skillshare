b97da69363f02a2f028e781107215b965e39f3e2
mwirman2 <mwirman2@US-5CD0144SQ9>
2021-07-03 16:37:49 +0800

    Update Patch

--- 08. Java Persistance API/mysqlquery.sql
+++ 08. Java Persistance API/mysqlquery.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE bookstoredb;
+CREATE DATABASE onlineshoppingdb;
 
-GRANT ALL PRIVILEGES ON bookstoredb.* TO 'user'@'%'''
+GRANT ALL PRIVILEGES ON bookstoredb.* TO 'user'@'%';
+GRANT ALL PRIVILEGES ON onlineshoppingdb.* TO 'user'@'%';

25894986009b3937997e1af9fc76802ce56a8ee1
dista.reza <mwirman2@dxc.com>
2021-11-19 17:33:08 +0800

    add new tutorial

---
+++ 10. Spring Boot Microservice/springinitializr/src/main/java/com/springboot/springinitializr/controller/MessageProperties.java
@@ -0,0 +1,20 @@
+package com.springboot.springinitializr.controller;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("messages")
+public class MessageProperties {
+
+	private String greeting = "Good Morning";
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+	
+}
